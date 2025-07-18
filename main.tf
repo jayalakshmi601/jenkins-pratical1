0231f3b051673e99690ef5f891c3c40f09afb699
jayalakshmi601 <152255852+jayalakshmi601@users.noreply.github.com>
2025-07-18 14:29:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws"{
 region = "us-east-2"
 }
-resource "aws_iam_user" "jaya" {
-jaya = "jaya_jaya_04"
+resource "aws_iam_user" "name" {
+name = "jaya_jaya_04"
 }
-resource "aws_iam_user" "prem" {
-prem = "jaya_jaya04"
+resource "aws_iam_user" "name" {
+name = "jaya_jaya04"
 }
 
